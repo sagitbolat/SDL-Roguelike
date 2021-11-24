5f111d612971ae1c553386021e3ffdf547ccdb9a
Sagit Bolat <sagitbolat@gmail.com>
2021-11-23 20:21:11 -0800

    added key press support

--- SDLtest/inputHandler.cpp
+++ SDLtest/inputHandler.cpp
@@ -1,41 +1,16 @@
 #include "inputHandler.h"
+#include <iostream>
 
 namespace input {
-
     /*
         Translates a SDL_Event into a input::Key enum and returns the enum value
     */
     Key PollInput(SDL_Event inputEvent) {
 
-        /*switch (inputEvent.type) {
-        case SDL_QUIT:
-            return input::Key::QUIT;
-        case SDL_KEYDOWN:
-            SDL_Keycode pressed = inputEvent.key.keysym.sym;
-            switch (pressed) {
-            case SDLK_w:
-                return Key::W;
-            case SDLK_a:
-                return Key::A;
-            case SDLK_s:
-                return Key::S;
-            case SDLK_d:
-                return Key::D;
-            case SDLK_ESCAPE:
-                return Key::ESCAPE;
-            case SDLK_RETURN:
-                return Key::ENTER;
-            case SDLK_TAB:
-                return Key::TAB;
-            default:
-                return Key::UNKNOWN;
-            }
-        case SDL_KEYUP:
-            return Key::UNKNOWN;
-        }*/
-
         if (inputEvent.type == SDL_QUIT) return input::Key::QUIT;
-        else if (inputEvent.type == SDL_KEYUP) return Key::RELEASE;
+        else if (inputEvent.type == SDL_KEYUP) { 
+            return Key::RELEASE; 
+        }
         else if (inputEvent.type == SDL_KEYDOWN) {
             SDL_Keycode pressed = inputEvent.key.keysym.sym;
             switch (pressed) {
